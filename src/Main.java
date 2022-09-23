3190a490825cb7a3b207d8be59aa8e3042819a74
zemscky <aleksei.zemscky@yandex.ru>
2022-09-23 16:25:53 +0300

    Домашняя работа 2, задание 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 //        Домашняя работа 2
-//        Задание 1
+//        Задание 1,2
         System.out.println("Задание 1");
         int contribution = 15_000; // Вклад
         double accumulation = 0; // Накопления в месяц
@@ -9,7 +9,13 @@ public class Main {
         int i = 1;
         for (;accumulation <= 12_000_000; i++) {
             accumulation = (accumulation + contribution) * (1 + precent);
+            if (i % 6 ==0){
             System.out.println(i + " месяц, накопления составляют: " + String.format("%.2f", accumulation) + " рублей" );
+            }
         }
+        System.out.println("Всего понадобиться " + i + " месяцев");
+
+
     }
+
 }
